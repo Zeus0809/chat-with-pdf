ccf600433bca7b1abd65af3eb4c37e8e082c68dd
Zeus0809 <ikkit2002@gmail.com>
2025-05-03 21:19:00 -0400

    Set up a simple conversation loop for testing the model.

--- llama.cpp_test.py
+++ llama.cpp_test.py
@@ -1,10 +1,19 @@
 from llama_cpp import Llama
 import pprint
 
-my_local_llm = Llama(model_path="./local_models/text/Yi-1.5-9B-Chat-IQ4_NL.gguf", chat_format="llama-2")
+my_local_llm = Llama(model_path="./local_models/text/Yi-1.5-9B-Chat-IQ4_NL.gguf",
+                     chat_format="chatml",
+                     n_ctx=4096,
+                     n_threads=8,
+                     use_mlock=False,
+                     verbose=False)
 
-messages = [ {"role": "system", "content": "You are a helpful multilingual assistant."}, {"role": "user", "content": "Hello Yi."} ]
+go_on = "y"
+while go_on == "y":
 
-response = my_local_llm.create_chat_completion_openai_v1(messages)
+    question = input("Ask me a question: ")
+    messages = [ {"role": "system", "content": "You are a helpful multilingual assistant"}, {"role": "user", "content": question} ]
+    response = my_local_llm.create_chat_completion(messages, max_tokens=200)
+    print(response["choices"][0]["message"]["content"])
+    go_on = input("\n\nGo on? y/n")
 
-print(response.choices[0].message.content)
