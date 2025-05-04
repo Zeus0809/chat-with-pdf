f9aed4cc5e89eba47c88fff50cbaa91a4acba6b6
Zeus0809 <ikkit2002@gmail.com>
2025-05-04 18:21:19 -0400

    Still looking for an optimal model that supports English-Russian-Ukrainian. Currently using Mistral-7B-Instruct.

--- llama.cpp_test.py
+++ llama.cpp_test.py
@@ -1,18 +1,19 @@
 from llama_cpp import Llama
 import pprint
 
-my_local_llm = Llama(model_path="./local_models/text/Yi-1.5-9B-Chat-IQ4_NL.gguf",
+my_local_llm = Llama(model_path="./local_models/text/mistral-7b-instruct-v0.1.Q5_0.gguf",
                      chat_format="chatml",
                      n_ctx=4096,
                      n_threads=8,
                      use_mlock=False,
-                     verbose=False)
+                     verbose=False
+                    )
 
 go_on = "y"
 while go_on == "y":
 
     question = input("Ask me a question: ")
-    messages = [ {"role": "system", "content": "You are a helpful multilingual assistant"}, {"role": "user", "content": question} ]
+    messages = [ {"role": "system", "content": "You are a helpful multilingual assistant. Respond in the language that user talks to you in."}, {"role": "user", "content": question} ]
     response = my_local_llm.create_chat_completion(messages, max_tokens=200)
     print(response["choices"][0]["message"]["content"])
     go_on = input("\n\nGo on? y/n")
